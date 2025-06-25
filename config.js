a7f4be3a296ee949b07ace611b7a001252a9187c
machinecrew <machinecrew2023@gmail.com>
2025-06-25 12:08:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const quizData = {
-  question: "Nama Panjang Nadzif?",
-  answer: "Nadzif Al Aqol",
-  rewardLink: "https://dana.com/hadiahnya"
+  question: "Rais",
+  answer: "Rais",
+  rewardLink: "https://google.com/hadiahnya"
 };

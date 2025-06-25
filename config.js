58c0cedbede81dfac1e53493023061ed62646cc6
machinecrew <machinecrew2023@gmail.com>
2025-06-25 13:41:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 const quizData = {
   question: "Kita di Ruang 4, Telpon Berdenging minta kita NAIK sekali, terus BELOK kanan, sekarang kita di Ruang Berapa?",
   answer: "Ruang 2",
-  rewardLink: "https://link.dana.id/danakaget?c=s45gckkdh&r=c20oRj&orderId=20250625101214477015010300166550228948248"
+  rewardLink: "https://link.dana.id/danakaget?c=s45gckkdh&r=c20oRj&orderId=20250625101214477015010300166550228948248",
+  clue: "Telpon Berdering ☎️"
 };

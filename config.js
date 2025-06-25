5b307e1e2532505d119fd8d507efda8690c29230
machinecrew <machinecrew2023@gmail.com>
2025-06-25 14:44:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const quizData = {
-  question: "Kalo Prabowo = 90 , Kalo Alifianti Indah Setyowati = ?",
-  answer: "254",
+  question: "Sekarang kita di Depok, 3 langkah sampai Gorontalo, dari Gorontalo 3 langkah lagi sampai mana?",
+  answer: ["Rais", "rais", "Rais R", "rais r"], // ✔️ Semua ini dianggap benar
   rewardLink: "https://link.dana.id/danakaget?c=sz9spvmpw&r=c20oRj&orderId=20250625101214839515010300166550228989555",
-  clue: "TOTAL 🔢"
+  clue: "Tiga Langkah Sampai🚶"
 };

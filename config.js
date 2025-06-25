91c3e5cafdc1814df1ad8c3cd751616ed7ed4b03
machinecrew <machinecrew2023@gmail.com>
2025-06-25 13:28:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const quizData = {
-  question: "Peler",
-  answer: "Peler",
+  question: "Kita di Ruang 4, Handphone berdengin minta kita NAIK sekali, BELOK kanan, sekarang kita di Ruang Berapa?",
+  answer: "Ruang 2", "2",
   rewardLink: "https://google.com/hadiahnya"
 };

82b9baf09194d449b6346969b024112962016dbf
machinecrew <machinecrew2023@gmail.com>
2025-06-25 13:34:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const quizData = {
-  question: "Kita di Ruang 4, Handphone Berdenging minta kita NAIK sekali, terus BELOK kanan, sekarang kita di Ruang Berapa?",
+  question: "Kita di Ruang 4, Telpon Berdenging minta kita NAIK sekali, terus BELOK kanan, sekarang kita di Ruang Berapa?",
   answer: "Ruang 2",
   rewardLink: "https://link.dana.id/danakaget?c=s45gckkdh&r=c20oRj&orderId=20250625101214477015010300166550228948248"
 };

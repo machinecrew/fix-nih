43fbd5cc03c1e83ac1ecd1e601213cd3188ae944
machinecrew <machinecrew2023@gmail.com>
2025-07-01 09:48:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 const quizData = {
   question: "Sekarang kita di Depok, 3 langkah lagi sampai Gorontalo, dari Gorontalo 3 langkah lagi sampai mana?",
   answer: ["Jakarta", "Jambi", "Jogja", "Jatinangor", "Jombang", "Jember", "Jepara", "Jayapura" ], // ✔️ Semua ini dianggap benar
-  rewardLink: "https://link.dana.id/danakaget?c=sz9spvmpw&r=c20oRj&orderId=20250625101214839515010300166550228989555",
+  rewardLink: "https://app.u.shopeepay.co.id/u/AVYSe9bTgBmJXgGeivcmE",
   clue: "Tiga Langkah Sampai🚶"
 };

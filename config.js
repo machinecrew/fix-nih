d7cadf4e774504e05653768f006364628664c99e
machinecrew <machinecrew2023@gmail.com>
2025-09-28 02:55:53 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const quizData = {
-  question: "Start dari Malang, tiga langkah sampe Palembang, dari Palembang 3 langkah lagi dimana?",
-  answer: ["Surabaya", "Semarang", "Solo", "Serang", "Salatiga", "Sidoarjo", "Sibolga", "Sorong" ], // ✔️ Semua ini dianggap benar
-  rewardLink: "https://app.u.shopeepay.co.id/u/gWRarfhV18uP4ruVuXCQn",
-  clue: "Tiga Langkah Sampai🚶"
-};
